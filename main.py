fc05be1eed7728b8d219a32c819f1112eeb4b07a
Marton Zelina <marton_zelina@yahoo.com>
2026-06-25 16:13:59 +0200

    change structure so Flask app can see templates and static files

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ This is the entry point for web server, built with Flash.
 
 import os
 from dotenv import load_dotenv, find_dotenv
-from flask import Flask
+from flask import Flask, render_template
 
 load_dotenv(find_dotenv())
 
@@ -12,17 +12,13 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def home():
     """
     The method when landing on the top page.
     """
     _ = os.getenv('X') # X variable is pass from Docply, placeholder.
 
-    crt_dir = os.path.abspath(os.getcwd())
-    with open(crt_dir + "/index.html", "r", encoding="utf-8") as f:
-        return f.read()
-    # return f"Hello, aaa!<ul>{env1}</ul><ul>{env2}</ul>"
-
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=80)
