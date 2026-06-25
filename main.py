47b97187ff1147e9ae2cef5279d1981693e3c3cb
Marton Zelina <marton_zelina@yahoo.com>
2026-06-25 14:58:37 +0200

    fix pylint

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+"""
+This is the entry point for web server, built with Flash.
+"""
+
 import os
 from dotenv import load_dotenv, find_dotenv
 from flask import Flask
@@ -9,8 +13,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    env1 = os.getenv('HOME')
-    env2 = os.getenv('X')
+    """
+    The method when landing on the top page.
+    """
+    _ = os.getenv('X') # X variable is pass from Docply
 
     crt_dir = os.path.abspath(os.getcwd())
     with open(crt_dir + "index.html", "r", encoding="utf-8") as f:
