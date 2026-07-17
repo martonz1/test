f4a9abb8801bd2bd0aca9f971ccccf1e7256f66e
Marton Zelina <101954731+martonz1@users.noreply.github.com>
2026-07-17 17:53:33 +0200

    Update API URL to use HTTPS

--- static/js/game_dutch.js
+++ static/js/game_dutch.js
@@ -1,4 +1,4 @@
-  const API = 'http://${window.location.hostname}:4000/api';
+  const API = 'https://${window.location.hostname}:4000/api';
   let currentLesson = null;
   let translationVisible = false;
 
